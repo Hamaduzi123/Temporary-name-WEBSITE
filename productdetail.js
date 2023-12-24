6797dc6df466fecf40fc1da75b2c2518bf9e173f
Hamaduzi123 <129594915+Hamaduzi123@users.noreply.github.com>
2023-12-24 18:10:32 +0300

    fix

--- productdetail.js
+++ productdetail.js
@@ -16,3 +16,31 @@ function goBack() {
         navbarLinks.classList.toggle('active');
         toggleButton.classList.toggle('active');
     });
+
+    document.addEventListener('DOMContentLoaded', function () {
+        // Retrieve product details from sessionStorage
+        const storedProductDetails = sessionStorage.getItem('productDetails');
+        if (storedProductDetails) {
+            const productDetails = JSON.parse(storedProductDetails);
+
+            // Replace placeholders with stored values
+            document.getElementById('product-detail-brand').textContent = productDetails.brand;
+            document.getElementById('product-detail-title').textContent = productDetails.title;
+            document.getElementById('product-detail-price').textContent = productDetails.price;
+            
+            // Set the content of the description with HTML tags
+            const descriptionContainer = document.getElementById('product-description');
+            descriptionContainer.innerHTML = productDetails.description;
+
+            document.getElementById('product-detail-image').src = productDetails.imageUrl;
+
+            // Generate star icons
+            const starsContainer = document.getElementById('product-detail-stars');
+            starsContainer.innerHTML = generateStarIcons(productDetails.stars);
+        }
+    });
+
+    function generateStarIcons(count) {
+        // Generate the HTML for the star icons based on the count
+        return '⭐'.repeat(count);
+    }
