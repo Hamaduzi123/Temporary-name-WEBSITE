775111abad75262a10393e9464d8babbe4023e87
Hamaduzi123 <129594915+Hamaduzi123@users.noreply.github.com>
2023-12-24 15:02:40 +0300

    form test

--- api/process-form.js
+++ api/process-form.js
@@ -1,21 +1,40 @@
 // api/process-form.js
+const nodemailer = require('nodemailer');
+
 module.exports = async (req, res) => {
     if (req.method === 'POST') {
         const { name, email, subject, message } = req.body;
 
         // You can add code here to validate and process the form data
 
-        // Send email (example using a hypothetical email sending module)
-        sendEmail(name, email, subject, message);
-
-        res.status(200).json({ success: true });
+        // Send email using nodemailer
+        try {
+            await sendEmail(name, email, subject, message);
+            res.status(200).json({ success: true });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
     } else {
         res.status(405).json({ error: 'Method Not Allowed' });
     }
 };
 
-// Example email sending function
-function sendEmail(name, email, subject, message) {
-    // Implement email sending logic here
-    // You might want to use a third-party service or your own SMTP server
+async function sendEmail(name, email, subject, message) {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_PASSWORD,
+        },
+    });
+
+    const mailOptions = {
+        from: process.env.GMAIL_USER,
+        to: 'alansariabdulla17@gmail.com', 
+        subject: subject,
+        text: `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`,
+    };
+
+    return transporter.sendMail(mailOptions);
 }
