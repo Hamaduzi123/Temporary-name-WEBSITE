54b9620b27abcaa6330f371a602b22540e733ca0
Hamaduzi123 <129594915+Hamaduzi123@users.noreply.github.com>
2023-12-24 15:07:12 +0300

    Update process-form.js

--- api/process-form.js
+++ api/process-form.js
@@ -1,24 +1,22 @@
-// api/process-form.js
-const nodemailer = require('nodemailer');
-
 module.exports = async (req, res) => {
     if (req.method === 'POST') {
         const { name, email, subject, message } = req.body;
 
-        // You can add code here to validate and process the form data
-
-        // Send email using nodemailer
         try {
+            // Attempt to send email
             await sendEmail(name, email, subject, message);
             res.status(200).json({ success: true });
         } catch (error) {
+            // Log the error
             console.error(error);
-            res.status(500).json({ error: 'Internal Server Error' });
+            // Return a more descriptive error message
+            res.status(500).json({ error: 'Failed to send email' });
         }
     } else {
         res.status(405).json({ error: 'Method Not Allowed' });
     }
 };
+
 
 async function sendEmail(name, email, subject, message) {
     const transporter = nodemailer.createTransport({
